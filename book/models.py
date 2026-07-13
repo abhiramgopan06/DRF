475326fede0ef2dacffd9d9d85c37e94daa49724
abhiram <abhiramgopan06@gmail.com>
2026-07-13 10:34:19 +0530

    permissions

--- book/models.py
+++ book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=30)
@@ -7,3 +8,12 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.author}"
+    
+class Task(models.Model):
+    title = models.CharField()
+    description = models. TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.title
+    
--- book/permissions.py
+++ book/permissions.py
@@ -5,3 +5,13 @@ class IsOwnerOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return obj.owner == request.user
+    
+class IsAdminOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.owner == request.user
+
+# ADMIN - allow
+# OWNER - allow
+# random user - disallow
